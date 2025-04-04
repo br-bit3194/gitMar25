4cce7df54927216e489ec8feda8a2085750dbe4a
br-bit3194 <bhavesh3194@gmail.com>
2025-04-04 22:44:27 +0530

    get particular product by id

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from products import views
 from django.urls import path
 urlpatterns = [
     path("hello/", views.hello),
-    path("products/", views.get_products),
+    path("allproducts/", views.get_products),
+    path("product/<int:id>/", views.get_product),
 ]
